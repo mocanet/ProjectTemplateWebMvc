a628e9e6eeecf75b638a57948060afdef6da5e8f
MiYABiS <miyabis@live.jp>
2018-01-28 06:34:45 +0900

    file del

--- ProjectTemplateWebMvc/content/My Project/MocaAssemblyInfo.vb
+++
@@ -1,5 +0,0 @@
-﻿
-<Assembly: WebActivatorEx.PreApplicationStartMethod(GetType(Moca.Di.MocaContainerFactory), "Init", Order:=1)>
-<Assembly: WebActivatorEx.PreApplicationStartMethod(GetType(Moca.Web.Mvc.MocaControllerFactory), "Init", Order:=2)>
-<Assembly: WebActivatorEx.PreApplicationStartMethod(GetType(Moca.Configuration.SectionProtector), "Protect", Order:=3)>
-<Assembly: WebActivatorEx.ApplicationShutdownMethod(GetType(Moca.Di.MocaContainerFactory), "Destroy")>
--- ProjectTemplateWebMvc/content/Properties/MocaAssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Moca.Di.MocaContainerFactory), "Init", Order = 1)]
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Moca.Web.Mvc.MocaControllerFactory), "Init", Order = 2)]
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Moca.Configuration.SectionProtector), "Protect", Order = 3)]
-[assembly: WebActivatorEx.ApplicationShutdownMethod(typeof(Moca.Di.MocaContainerFactory), "Destroy")]
